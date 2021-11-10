d78bc55dd77f41d32772d499556dfe98331fc8ed
JoaquinT04 <JoaquinT04@users.noreply.github.com>
2021-11-10 23:58:52 +0000

    deploy: 95be7dc0b9fae46ec209fe47935d605ff2aab127

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,9 +1,23 @@
 /**
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+**/
+const baseUrl = "https://platzi-avo.vercel.app/"; 
 
-const url = "https://platzi-avo.vercel.app/api/avo";
+const appNode = document.querySelector('#app');
+
+const formatPrice = (price) => {
+	
+	const newPrice = new window.Intl.NumberFormat('en-EN', {
+		style: 'currency',
+		currency: 'USD',
+	}).format(price)
+
+	return newPrice;
+};
+// API intl (internacionalizacion en ingles);
+// 1 - format dat
+// 2 - format monedas
 
 // web api
 // Conectarnos al servidor
@@ -21,7 +35,7 @@ const url = "https://platzi-avo.vercel.app/api/avo";
 		
 // Conectarnos al servidor Con Async y await
 async function recolectandoDatosApi(){
-	let response = await fetch(url);
+	let response = await fetch(`${baseUrl}api/avo`);
 	let dataJson = await response.json()
 	const todosLosItems= [];
 	console.log("fetch: ", response);	
@@ -29,19 +43,36 @@ async function recolectandoDatosApi(){
 	console.log("length =",dataJson.length);
 	dataJson.data.forEach(item => {
 		// crear imagen
+		
 		const image = document.createElement("img");
+		
+		//asigno la url de la API como src de la etiqueta
+		image.src = `${baseUrl}${item.image}`;
+
 		// crear Titulo
 		const title = document.createElement("h2")
+		
+		//asigno el name de la API como contenido de la etiqueta
+		title.textContent = item.name;
+	
+		// Agrego una clase a title
+		title.className = "muy-grande";
+
 		// crear Precio
 		const price = document.createElement("div");
+		
+		//asigno el precio de la API como contenido de la etiqueta
+		price.textContent = formatPrice(item.price);
+
+
 		// Creo un contenedor para guardar todo lo anterior
 		const container = document.createElement("div");
-		container.appendChild(image, title, price);
+		container.append(image, title, price);
 
 		todosLosItems.push(container);
 	});
 
-	document.body.append(...todosLosItems);
+	appNode.append(...todosLosItems);
 
 }
 
